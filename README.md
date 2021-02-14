fe9d8ed07f41745f526ba6fb083c63d3364ab2c7
bananabread08 <rmkfermin@gmail.com>
2021-02-14 15:00:35 +0800

    fixed Tie condition, removed form.

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 ##Tic-Tac-Toe Project
 
 A project to showcase what we learned about Objects and apply Factory Functions & Modules in the code skeleton.
+
+Functionality:
+1. Play a TTT game. Human vs. Human. (Will add AI soon).
+2. Show's whose turn it is via gamestate.
+3. Reset button to refresh game. 
+4. Show winner / tie if Win Conditions are passed.
+
+What I learned: 
+1. OOP especially about Factory Functions and Modules. 
+2. Closures!
+3. The `{once: true}` event options. 
+4. Some JS & CSS tricks using `display: none` to make a pseudo page 2 or page 3++ of webpage.
+5. Did I mention OOP? 
+
+Future update: Implement minimax Algorithm to make Unbeatable AI (hopefully!). 
--- scripts/ttt.js
+++ scripts/ttt.js
@@ -1,112 +1,114 @@
-const Player = (name, mark) => { //player factory
+const Player = (name, mark) => {
     return { name, mark };
   }
   
-const gameboard = (() => { //initialize board values
-  let boardArray = ["", "", "", "", "", "", "", "", ""];
-  let winCondition = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-  let p1Turn = true;
-  return { boardArray, winCondition, p1Turn };
-})();
+  //global lol
+  let playerOne = Player("Player 1", "X");
+  let playerTwo = Player("Player 2", "O");
   
-const displayController = (() => {
-  const updateBoard = (e, mark, board) => {
-    board[e.target.getAttribute("data-num")] = mark;
-    e.target.innerText = `${mark}`;
-    e.target.classList.remove("grid-hover");
-    return board;
-  }
-
-  const showCurrentPlayer = (state, playerName, mark) => {
-    state.innerText = `It's ${playerName}'s turn. ${mark}`;
-  }
-
-  function refreshPage() {
-    window.location.reload();
-  }
-
-  const isGameOver = (winMsg) => {
-    gameflow.secondPage.style.display = "none";
-    gameflow.gameoverEl.style.display = "flex";
-    gameflow.gameoverEl.appendChild(gameflow.state);
-    gameflow.gameoverEl.appendChild(gameflow.reset);
-  }
-
-  return { updateBoard, showCurrentPlayer, refreshPage, isGameOver };
-})();
+  const gameboard = (() => {
+    //initialize board values
+    let boardArray = ["", "", "", "", "", "", "", "", ""];
+    let winCondition = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    let p1Turn = true;
+    return { boardArray, winCondition, p1Turn };
+  })();
   
-const gameflow = (() => {
-  //vars & selectors &
-  let xIsNext = gameboard.p1Turn;
-  let board = gameboard.boardArray;
-  const squares = document.querySelectorAll("#squares");
-  const state = document.querySelector(".game-state");
-  const reset = document.querySelector("#reset");
-  const play = document.querySelector(".play-btn");
-  const firstPage = document.querySelector(".start-game");
-  const secondPage = document.querySelector(".play-game");
-  const gameoverEl = document.querySelector(".game-over");
-  const p1El = document.getElementById("p1-name").value;
-  const p2El = document.getElementById("p2-name").value;
-  const playerOne = Player(p1El, "X");
-  const playerTwo = Player(p2El, "O");
-
-  state.innerText = `It's ${playerOne.name}'s turn. ${playerOne.mark}`; //initial gamestate
-
-  //events
-  squares.forEach(square => {
-    square.addEventListener("click", (e) => { handleClick(e) }, { once: true }); //one click per square only
-  });
-  reset.addEventListener("click", displayController.refreshPage);
-  play.addEventListener("click", () => {
-    startGame();
-  });
-  
-  //func()
-  const startGame = () => {
-    firstPage.style.display = "none";
-    secondPage.style.display = "flex";
-  }
-
-  const handleClick = (e) => {
-    if (xIsNext) {
-      e.target.classList.add("X");
-      displayController.updateBoard(e, playerOne.mark, board);
-      displayController.showCurrentPlayer(state, playerTwo.name, playerTwo.mark);
-      xIsNext = false;
-      checkWinner(board, state);
+  const displayController = (() => {
+    const updateBoard = (e, mark, board) => {
+      board[e.target.getAttribute("data-num")] = mark;
+      e.target.innerText = `${mark}`;
+      e.target.classList.remove("grid-hover");
+      return board;
     }
-    else if (!xIsNext) {
-      e.target.classList.add("O");
-      displayController.updateBoard(e, playerTwo.mark, board);
-      displayController.showCurrentPlayer(state, playerOne.name, playerOne.mark);
-      xIsNext = true;
-      checkWinner(board, state);
+  
+    const showCurrentPlayer = (state, playerName, mark) => {
+      state.innerText = `It's ${playerName}'s turn. ${mark}`;
     }
-  }
+  
+    function refreshPage() {
+      window.location.reload();
+    }
 
-  const checkWinner = (board, state) => {
-    let win = gameboard.winCondition;
-    let fullBoardCheck = board.filter(element => element === "X" || element ==="O");
-    return (win.forEach(element => {
-      let checker = "";
-      for (let index = 0; index < 3; index++) {
-        checker += board[element[index]];
+    const isGameOver = () => {
+      gameflow.secondPage.style.display = "none";
+      gameflow.gameoverEl.style.display = "flex";
+      gameflow.gameoverEl.appendChild(gameflow.state);
+      gameflow.gameoverEl.appendChild(gameflow.reset);
+    }
+  
+    return { updateBoard, showCurrentPlayer, refreshPage, isGameOver };
+  })();
+  
+  const gameflow = (() => {
+    //vars & selectors &
+    let xIsNext = gameboard.p1Turn;
+    let board = gameboard.boardArray;
+    const squares = document.querySelectorAll("#squares");
+    const state = document.querySelector(".game-state");
+    const reset = document.querySelector("#reset");
+    const play = document.querySelector(".play-btn");
+    const firstPage = document.querySelector(".start-game");
+    const secondPage = document.querySelector(".play-game");
+    const gameoverEl = document.querySelector(".game-over");
+  
+    state.innerText = `It's ${playerOne.name}'s turn. ${playerOne.mark}`; //initial gamestate
+
+    //events
+    squares.forEach(square => {
+      square.addEventListener("click", (e) => { handleClick(e) }, { once: true }); //one click per square only
+    });
+    reset.addEventListener("click", displayController.refreshPage);
+    play.addEventListener("click", () => {
+      startGame();
+    });
+    
+    //func()
+    const startGame = () => {
+      firstPage.style.display = "none";
+      secondPage.style.display = "flex";
+    }
+  
+    const handleClick = (e) => {
+      if (xIsNext) {
+        e.target.classList.add("X");
+        displayController.updateBoard(e, playerOne.mark, board);
+        displayController.showCurrentPlayer(state, playerTwo.name, playerTwo.mark);
+        xIsNext = false;
+        checkWinner(board, state);
       }
-      if (checker === "XXX") {
-        state.innerText = `${playerOne.name} wins! ${playerOne.mark}`;
-        return displayController.isGameOver(state);
+      else if (!xIsNext) {
+        e.target.classList.add("O");
+        displayController.updateBoard(e, playerTwo.mark, board);
+        displayController.showCurrentPlayer(state, playerOne.name, playerOne.mark);
+        xIsNext = true;
+        checkWinner(board, state);
       }
-      else if (checker === "OOO") {
-        state.innerText = `${playerTwo.name} wins! ${playerOne.mark}`;
-        return displayController.isGameOver(state);
-      }
-      else if (fullBoardCheck.length === 9 && (checker !== "XXX" && checker !== "OOO")){
-        state.innerText = "It's a tie! Reset to play again.";
-        return displayController.isGameOver(state);
-      }
-      else return;
-    }));
-  }
-  return {gameoverEl, secondPage, state, reset};
-})();
+    }
+  
+    const checkWinner = (board, state) => {
+      let win = gameboard.winCondition;
+      let fullBoardCheck = board.filter(element => element === "X" || element ==="O");
+      return (win.forEach(element => {
+        let checker = "";
+        for (let index = 0; index < 3; index++) {
+          checker += board[element[index]];
+        }
+        if (checker === "XXX") {
+          state.innerText = `${playerOne.name} wins! ${playerOne.mark}`;
+          return displayController.isGameOver();
+        }
+        else if (checker === "OOO") {
+          state.innerText = `${playerTwo.name} wins! ${playerTwo.mark}`;
+          return displayController.isGameOver();
+        }
+        else if (fullBoardCheck.length === 10 && (checker !== "XXX" && checker !== "OOO")){
+          state.innerText = "It's a tie! Reset to play again.";
+          return displayController.isGameOver();
+        }
+        else return;
+      }));
+    }
+
+    return {gameoverEl, secondPage, state, reset};
+  })();
